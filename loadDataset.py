56377bf531405d38ee76de85412c429edcb2679a
Andrew Lau <andrewlau2019@gmail.com>
2026-01-11 16:21:39 -0800

    setup

---
+++ loadDataset.py
@@ -0,0 +1,20 @@
+import os
+from constants import *
+from dataset import MAESTRO
+
+def loadDataset(dataset_root):
+    print(f"Initializing MAESTRO dataset from {dataset_root}...")
+    dataset = MAESTRO(path=dataset_root, groups=['train'], sequence_length=SEQUENCE_LENGTH)
+
+    if len(dataset) == 0:
+        print("Error: Dataset is empty. Check your path and metadata")
+        return 
+
+if __name__ == "__main__":
+    print(f"Using device: {DEVICE}")
+
+    if os.path.exists(DATA_PATH):
+        loadDataset(DATA_PATH)
+    else:
+        print(f"Path not found: {DATA_PATH}")
+        print("Please edit DATA_PATH in loadDataset.py")
