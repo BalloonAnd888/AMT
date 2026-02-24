e12277e1df6ca8f8c2ab4130e7d92c65be0d5351
Andrew Lau <andrewlau2019@gmail.com>
2026-02-23 20:11:28 -0800

    Added byte dance model

--- models/pianotranscriptionbytedance/inference.py
+++ models/pianotranscriptionbytedance/inference.py
@@ -1,10 +1,34 @@
-from piano_transcription_inference import PianoTranscription, sample_rate, load_audio
+import os
+import torch 
+import librosa
+import urllib.request
+from piano_transcription_inference import PianoTranscription, sample_rate
 
-# Load audio
-(audio, _) = load_audio('resources/cut_liszt.mp3', sr=sample_rate, mono=True)
+def inference(audio_path):
+    # Load audio
+    print(audio_path)
+    audio, _ = librosa.load(audio_path, sr=sample_rate, mono=True)
 
-# Transcriptor
-transcriptor = PianoTranscription(device='cuda')    # 'cuda' | 'cpu'
+    # Checkpoint path
+    checkpoint_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'note_F1=0.9677_pedal_F1=0.9186.pt')
+    if not os.path.exists(checkpoint_path):
+        print(f"Downloading checkpoint to {checkpoint_path}")
+        urllib.request.urlretrieve('https://zenodo.org/record/4034264/files/CRNN_note_F1=0.9677_pedal_F1=0.9186.pth', checkpoint_path)
 
-# Transcribe and write out to MIDI file
-transcribed_dict = transcriptor.transcribe(audio, 'cut_liszt.mid')
+    # Transcriptor
+    transcriptor = PianoTranscription(device='cuda', checkpoint_path=checkpoint_path)    # 'cuda' | 'cpu'
+
+    # Transcribe and write out to MIDI file
+    results_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'results')
+    os.makedirs(results_dir, exist_ok=True)
+    midi_path = os.path.join(results_dir, os.path.splitext(os.path.basename(audio_path))[0] + '.mid')
+    transcribed_dict = transcriptor.transcribe(audio, midi_path)
+
+if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    audio_dir = os.path.abspath(os.path.join(current_dir, '..', '..', 'audio'))
+
+    audio_path = os.path.join(audio_dir, 'route1.wav')
+
+    inference(audio_path)
+    
